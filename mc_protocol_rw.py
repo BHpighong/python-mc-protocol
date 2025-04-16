87f2bb29686f9cd145b668c80d78fcd55b09f536
bh.hong <91011946+BHpighong@users.noreply.github.com>
2025-04-16 11:16:56 +0700

    Update mc_protocol_rw.py

--- mc_protocol_rw.py
+++ mc_protocol_rw.py
@@ -14,32 +14,29 @@ class McProtocolRW:
         self.cr = '0D' #CR
         self.lf = '0A' #LF
         self.end_code = f"{self.cr} {self.lf}" #self.cr+' '+self.lf
+
         #Frame ID No. as below
         self.frame_id = '46 39' #F9(3C frame)
         self.station_no= '30 30'
         self.network_no= '30 30'
         self.pc_no='46 46'
         self.self_station_no= '30 30'
-        frame_code_component = [
+
+        self.frame_code = ' '.join([
             self.frame_id,
             self.station_no,
             self.network_no,
             self.pc_no,
             self.self_station_no
-        ]
-        self.frame_code = ' '.join(filter(None, frame_code_component)) #將所有的字串組合起來
+        ])
 
         #指令代碼
-        self.batch_read = 401
-        self.batch_write = 1401
+        # self.batch_read = '0401'
+        # self.batch_write = '1401'
 
     def device_code_define (self, device_code):
         """ 取得裝置代碼（例：'M', 'D'） """
-        start = format(ord('*'), 'X')
-        code_with_0x = format(ord(device_code), 'X')
-        device_code = code_with_0x + ' ' + start
-        #print(device_code)
-        return device_code
+        return f"{format(ord(device_code), 'X')} {format(ord('*'), 'X')}"
 
     def str_to_ascii (self, address_int, length):
         """定義from int to hex(Ascii),給定int,格子數"""
@@ -61,48 +58,23 @@ class McProtocolRW:
 
     def point_status_qty (self, point_status):
         """點位輸狀態；配合點位數量(實際輸出為先數量再狀態)"""
-        #point_status = [0, 1] #debug測試點
-        point_qty_int = len(point_status)
-        point_qty_in_hex = self.str_to_ascii(point_qty_int, 4)
+        qty_in_ascii = self.str_to_ascii(len(point_status), 4)
+        point_status_ascii = ' '.join([format(ord(str(s)), 'X') for s in point_status])
+        return f"{qty_in_ascii} {point_status_ascii}"
 
-        #list transfor from int to Ascii
-        point_status_ascii = [1]*point_qty_int #避免錯誤定預設1個，status in '0'
+    def data_qty_unit (self, words):
+        """ 當點位是D記存器且傳送內容為word時，傳入矩陣元素個數，自動生出點位數量 """
+        point_qty_ascii = self.str_to_ascii(len(words), 4)
+        data_ascii = ' '.join([self.str_to_ascii(f"{w:X}"[2:], 4) for w in words])
 
-        for i in range (point_qty_int):
-            point_status_ascii[i] = format(ord(str(point_status[i])), 'X')
-
-        point_in_str = ' '
-
-        return point_qty_in_hex+' '+point_in_str.join(point_status_ascii)
-
-    def data_qty_unit (self, word):
-        """
-            當點位是D記存器且傳送內容為word時
-            1. 傳入矩陣元素個數，自動生出點位數量
-        """
-        point_qty_int = len(word)
-        point_qty_in_hex = self.str_to_ascii(point_qty_int, 4)
-
-        i = 0
-        data_qty_and_words = ''
-
-        for i in range (point_qty_int) :
-            # 將 word[i] 轉為 4 字元長的 ASCII 字串，並加到結果字串中
-            ascii_str = f"0x{word[i]:X}"[2:]
-            data_qty_and_words = data_qty_and_words + f' {self.str_to_ascii(ascii_str, 4)}'
-        return point_qty_in_hex+data_qty_and_words
+        return f"{point_qty_ascii} {data_ascii}"
 
     def check_sum(self, data):
         """ 求CRC校驗碼 """
-        sumtest = 0 # sumtest intial
-        d = [int(x, 16) for x in data.split(' ')] # 將str轉list，並以int儲存
-        for value in d:  # 直接遍歷元素
-            sumtest += value  # 累加每個元素的值
-
-        code1 = f"{sumtest:X}"[-2]  # 將轉為 ASCII
-        code2 = f"{sumtest:X}"[-1]  # 將轉為 ASCII
-        #print (code1) #印出來確認values
-
+        sum_check_code = sum(int(x, 16) for x in data.split(' ')) # 將list的int加總
+        code1 = f"{sum_check_code:X}"[-2]  # 將轉為 ASCII
+        code2 = f"{sum_check_code:X}"[-1]  # 將轉為 ASCII
+        #print (code1, code1) #印出來確認values
         check_code = format(ord(code1), "x") +' '+ format(ord(code2), "x")
 
         return check_code
@@ -117,6 +89,29 @@ class McProtocolRW:
             data_payload = self.point_status_qty(values)
         elif dev_type == 'D':
             data_payload = self.data_qty_unit(values)
+        else:
+            raise ValueError("Unsupported device type")
+
+        payload = ' '.join([
+            self.frame_code,
+            self.str_to_ascii(command_code, 8),
+            device_code,
+            device_address,
+            data_payload
+        ])
+
+        return payload, device_code, device_address, data_payload
+
+    def build_command_payload_by_values(self, headdevice, values, command_code):
+        """ pay load building """
+        dev_type = headdevice[0]
+        device_code = self.device_code_define(dev_type)
+        device_address = self.str_to_ascii(headdevice[1:], 6)
+
+        if dev_type == 'M':
+            data_payload = self.str_to_ascii(values, 4) #self.point_status_qty(values)
+        elif dev_type == 'D':
+            data_payload = self.str_to_ascii(values, 4) #self.data_qty_unit(values)
         else:
             raise ValueError("Unsupported device type")
 
@@ -151,63 +146,43 @@ class McProtocolRW:
         payload, device_code, device_address, data_payload = self.build_command_payload(headdevice, values, command_code)
         return self.format_packet(payload, device_code, device_address, data_payload, command_code)
 
-    def command_random (self, headdevice, values):
+    def build_random_write_payload(self, headdevices, values, is_bit=True):
+        """ Build payload for random write command """
+        if len(headdevices) != len(values):
+            raise ValueError("Number of headdevices and values must match")
+
+        if is_bit:
+            command_code = '14020001'
+            qty = self.str_to_ascii(len(headdevices), 2)
+            word_pack = ''
+            for i, device in enumerate(headdevices):
+                device_code = self.device_code_define(device[0])
+                device_address = self.str_to_ascii(device[1:], 6)
+                status_ascii = self.str_to_ascii(values[i], 2)
+                word_pack += f" {device_code} {device_address} {status_ascii}"
+            crc_components = ' '.join([self.frame_code, self.str_to_ascii(command_code, 8), qty]) + word_pack
+        else:
+            command_code = '14020000'
+            qty_words = self.str_to_ascii(len(headdevices), 2)
+            qty_dwords = self.str_to_ascii(0, 2)
+            word_pack = ''
+            for i, device in enumerate(headdevices):
+                device_code = self.device_code_define(device[0])
+                device_address = self.str_to_ascii(device[1:], 6)
+                word_pack += f" {device_code} {device_address} {values[i]}"
+            crc_components = ' '.join([self.frame_code, self.str_to_ascii(command_code, 8), qty_words, qty_dwords]) + word_pack
+
+        crc = self.check_sum(crc_components)
+        packet = f"{self.enq_code} {crc_components} {crc} {self.end_code}"
+        return bytes.fromhex(packet)
+
+    def command_random(self, headdevices, values):
         """ Random_write """
-        i = 0
-        command_code = None
-        if len(headdevice) != len(values): #確保點位數量，與變數數量一致
-            print('Err Qty in points and values notmatch')
-            # exit()
-        else :
-            pass
-        #Writing data in bit units
-        if headdevice[0][0] == 'M': #目前針對M機碼
-            # print('Random writing data in bit M, Y...')
-            command_code= self.str_to_ascii(14020001, 8) # 1402, 0001
-            qty_of_points = self.str_to_ascii (len(headdevice), 2 )
-            word_pack = ''
+        is_bit = headdevices[0][0] == 'M'
+        return self.build_random_write_payload(headdevices, values, is_bit)
 
-            for i, device in enumerate(headdevice):
-                # 取得裝置代碼（例：'M', 'D'）
-                device_code = self.device_code_define(device[0])
-                device_address = self.str_to_ascii(device[1:], 6) #address6位
-                device_ststus = self.str_to_ascii(values[i], 2)
-                word_pack = word_pack +' ' +device_code + ' '+device_address +' '+device_ststus
-
-            crc_components = [
-                self.frame_code,
-                command_code,
-                qty_of_points
-            ]
-            code_crc = ' '.join(filter(None, crc_components)) + word_pack
-            crc_code = self.check_sum (code_crc)
-            command_code = self.enq_code+' '+code_crc+' '+crc_code+' '+self.end_code
-
-        elif headdevice[0][0] == 'D' :
-            # print('Random writing data in word D, W...')
-            command_code= self.str_to_ascii(14020000, 8) #1402, 0001
-            qty_of_words = self.str_to_ascii (len(headdevice), 2 )
-            qty_of_double_words = self.str_to_ascii (0, 2 )
-            word_pack = ''
-
-            for i, device in enumerate(headdevice):
-                # 取得裝置代碼（例：'M', 'D'）
-                device_code = self.device_code_define(device[0])
-                device_address = self.str_to_ascii(device[1:], 6)  # address 6 位
-                device_ststus = values[i]  # 已經是 Ascii
-                word_pack = word_pack +' ' +device_code +' ' +device_address +' ' +device_ststus
-
-            crc_components = [
-                self.frame_code,
-                command_code,
-                qty_of_words,
-                qty_of_double_words
-            ]
-            code_crc = ' '.join(filter(None, crc_components)) + word_pack
-            crc_code = self.check_sum (code_crc)
-            command_code = self.enq_code+' '+code_crc+' '+crc_code+' '+self.end_code
-
-        else :
-            pass
-
-        return bytes.fromhex( command_code )
+    def command_batch_read (self, headdevice, values):
+        """ batch 讀取命令"""
+        command_code = '04010001' #0401, 0001
+        payload, device_code, device_address, data_payload = self.build_command_payload_by_values(headdevice, values, command_code)
+        return self.format_packet(payload, device_code, device_address, data_payload, command_code)
