fbdda5484d6ac52c94fd2a9858ceab5b868b17ec
bh.hong <91011946+BHpighong@users.noreply.github.com>
2025-04-17 08:38:04 +0700

    Update mc_protocol_rw.py
    優化，"pay_load"，函數命名

--- mc_protocol_rw.py
+++ mc_protocol_rw.py
@@ -1,7 +1,7 @@
 """
+目的 : 產出MC_Protocol 的指令
 file name : mc_protocol_rw.py
 Author: bh.hong
-目的 : 產出MC_Protocol 的指令
 """
 
 class McProtocolRW:
@@ -38,21 +38,23 @@ class McProtocolRW:
         """ 取得裝置代碼（例：'M', 'D'） """
         return f"{format(ord(device_code), 'X')} {format(ord('*'), 'X')}"
 
-    def str_to_ascii (self, address_int, length):
+    def to_ascii_hex_string (self, address_int, length):
         """定義from int to hex(Ascii),給定int,格子數"""
-        msg = str(address_int).zfill(length) #int >>str >>補0
-        return  ' '.join([format(ord(c), 'X') for c in msg]) #str >>ascii
+        str_hex_ascii = str(address_int).zfill(length) #int >>str >>補0
+        #將字串轉為ascii
+        return ' '.join([format(ord(code), 'X') for code in str_hex_ascii])
 
     def point_status_qty (self, point_status):
         """點位輸狀態；配合點位數量(實際輸出為先數量再狀態)"""
-        qty_in_ascii = self.str_to_ascii(len(point_status), 4)
+        qty_in_ascii = self.to_ascii_hex_string(len(point_status), 4)
         point_status_ascii = ' '.join([format(ord(str(s)), 'X') for s in point_status])
+
         return f"{qty_in_ascii} {point_status_ascii}"
 
     def data_qty_unit (self, words):
         """ 當點位是D記存器且傳送內容為word時，傳入矩陣元素個數，自動生出點位數量 """
-        point_qty_ascii = self.str_to_ascii(len(words), 4)
-        data_ascii = ' '.join([self.str_to_ascii(f"{w:X}"[2:], 4) for w in words])
+        point_qty_ascii = self.to_ascii_hex_string(len(words), 4)
+        data_ascii = ' '.join([self.to_ascii_hex_string(f"{w:X}"[2:], 4) for w in words])
 
         return f"{point_qty_ascii} {data_ascii}"
 
@@ -61,24 +63,18 @@ class McProtocolRW:
         sum_check_code = sum(int(x, 16) for x in data.split(' ')) # 將list的int加總
         code = f"{sum_check_code:X}"[-2:]
         # print(' '.join([format(ord(c), "x") for c in code]))
-        return ' '.join([format(ord(c), "x") for c in code]) #將ASCII轉為HEX
+        return ' '.join([format(ord(c), "x") for c in code])
 
     def build_command_payload(self, headdevice, values, command_code):
         """ pay load building """
         dev_type = headdevice[0]
         device_code = self.device_code_define(dev_type)
-        device_address = self.str_to_ascii(headdevice[1:], 6)
-
-        if dev_type == 'M':
-            data_payload = self.point_status_qty(values)
-        elif dev_type == 'D':
-            data_payload = self.data_qty_unit(values)
-        else:
-            raise ValueError("Unsupported device type")
+        device_address = self.to_ascii_hex_string(headdevice[1:], 6)
+        data_payload = self.pay_load_style(dev_type, values, command_code)
 
         payload = ' '.join([
             self.frame_code,
-            self.str_to_ascii(command_code, 8),
+            self.to_ascii_hex_string(command_code, 8),
             device_code,
             device_address,
             data_payload
@@ -86,28 +82,18 @@ class McProtocolRW:
 
         return payload, device_code, device_address, data_payload
 
-    def build_command_payload_by_values(self, headdevice, values, command_code):
-        """ pay load building """
-        dev_type = headdevice[0]
-        device_code = self.device_code_define(dev_type)
-        device_address = self.str_to_ascii(headdevice[1:], 6)
+    def pay_load_style(self, dev_type, values, command_code):
+        """" pay load style """
+        if str(command_code) == '04010001':
+            return self.to_ascii_hex_string(values, 4)
 
-        if dev_type == 'M':
-            data_payload = self.str_to_ascii(values, 4) #self.point_status_qty(values)
-        elif dev_type == 'D':
-            data_payload = self.str_to_ascii(values, 4) #self.data_qty_unit(values)
+        elif str(command_code) == '14010001':
+            if dev_type == 'M':
+                return self.point_status_qty(values)
+            elif dev_type == 'D':
+                return self.data_qty_unit(values)
         else:
             raise ValueError("Unsupported device type")
-
-        payload = ' '.join([
-            self.frame_code,
-            self.str_to_ascii(command_code, 8),
-            device_code,
-            device_address,
-            data_payload
-        ])
-
-        return payload, device_code, device_address, data_payload
 
     def format_packet(self, payload, device_code, device_address, data_payload, command_code):
         """ formate fine tuning """
@@ -115,7 +101,7 @@ class McProtocolRW:
         packet = ' '.join([
             self.enq_code,
             self.frame_code,
-            self.str_to_ascii(command_code, 8),
+            self.to_ascii_hex_string(command_code, 8),
             device_code,
             device_address,
             data_payload,
@@ -128,9 +114,9 @@ class McProtocolRW:
         """ batch 命令 """
         command_code = 14010001
         payload, device_code, device_address, data_payload = self.build_command_payload(headdevice,
-        values,
-        command_code
-        )
+                                                                                    values,
+                                                                                    command_code
+                                                                                    )
         return self.format_packet(payload, device_code, device_address, data_payload, command_code)
 
     def build_random_write_payload(self, headdevices, values, is_bit=True):
@@ -140,27 +126,27 @@ class McProtocolRW:
 
         if is_bit:
             command_code = '14020001'
-            qty = self.str_to_ascii(len(headdevices), 2)
+            qty = self.to_ascii_hex_string(len(headdevices), 2)
             word_pack = ''
             for i, device in enumerate(headdevices):
                 device_code = self.device_code_define(device[0])
-                device_address = self.str_to_ascii(device[1:], 6)
-                status_ascii = self.str_to_ascii(values[i], 2)
+                device_address = self.to_ascii_hex_string(device[1:], 6)
+                status_ascii = self.to_ascii_hex_string(values[i], 2)
                 word_pack += f" {device_code} {device_address} {status_ascii}"
             crc_components = ' '.join([self.frame_code,
-                                       self.str_to_ascii(command_code, 8),
+                                       self.to_ascii_hex_string(command_code, 8),
                                        qty]) + word_pack
         else:
             command_code = '14020000'
-            qty_words = self.str_to_ascii(len(headdevices), 2)
-            qty_dwords = self.str_to_ascii(0, 2)
+            qty_words = self.to_ascii_hex_string(len(headdevices), 2)
+            qty_dwords = self.to_ascii_hex_string(0, 2)
             word_pack = ''
             for i, device in enumerate(headdevices):
                 device_code = self.device_code_define(device[0])
-                device_address = self.str_to_ascii(device[1:], 6)
+                device_address = self.to_ascii_hex_string(device[1:], 6)
                 word_pack += f" {device_code} {device_address} {values[i]}"
             crc_components = ' '.join([self.frame_code,
-                                       self.str_to_ascii(command_code, 8),
+                                       self.to_ascii_hex_string(command_code, 8),
                                        qty_words, qty_dwords]) + word_pack
 
         crc = self.check_sum(crc_components)
@@ -175,10 +161,9 @@ class McProtocolRW:
     def command_batch_read (self, headdevice, values):
         """ batch 讀取命令"""
         command_code = '04010001' #0401, 0001
-        cmd_result = self.build_command_payload_by_values(headdevice,
-        values,
-        command_code
-        )
+        cmd_result= self.build_command_payload(headdevice,
+                                               values,
+                                               command_code)
 
         payload, device_code, device_address, data_payload = cmd_result
         return self.format_packet(payload, device_code, device_address, data_payload, command_code)
